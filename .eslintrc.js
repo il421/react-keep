1d41d73240af833b38417876b5018c37ce1d9751
Ilya Suglobov <il421@mail.ru>
2019-06-27 23:55:30 +1200

    Webpack changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     root: true,
     env: {
         'browser': true,
-        'es6': true
+        'es6': true,
+        'node': true,
     },
     extends: ['eslint:recommended', 'plugin:react/recommended'],
     globals: {
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,7 +12,9 @@ if (process.env.NODE_ENV === 'test') {
 
 module.exports = (env) => {
   const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
+  const minCssExtract = new MiniCssExtractPlugin({
+    filename: 'style.css',
+  });
 
   return {
     entry: ['@babel/polyfill', './src/app.js'],
@@ -20,15 +22,25 @@ module.exports = (env) => {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
+
+    stats: {
+      entrypoints: false,
+      children: false
+    },
+
     module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
           use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
             {
               loader: 'css-loader',
               options: {
@@ -42,11 +54,11 @@ module.exports = (env) => {
               }
             }
           ]
-        })
-      }]
+        }
+      ]
     },
     plugins: [
-      CSSExtract,
+      minCssExtract,
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
