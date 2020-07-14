088badaa9b19f4e228c1dcc432050ea32773511d
ilya.suglobov <ilya.suglobov@bpsoftware.net>
2020-07-15 00:24:02 +1200

    git config

--- src/testData/users.ts
+++ src/testData/users.ts
@@ -1,6 +1,6 @@
 export const user = {
   email: "app.ilya.testing@gmail.com",
-  password: "E4E2Mo12345678",
+  password: "E4E2Mo123456789",
   uid: "{SECURE_TEST_USER_UID}",
   firstName: "Ilya",
   lastName: "Suglobov",
